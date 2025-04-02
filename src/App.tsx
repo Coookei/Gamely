ad9782096fff286f0ff626403fd7deb78592f9d5
Coookei <89310401+Coookei@users.noreply.github.com>
2025-04-02 10:18:25 +0100

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@chakra-ui/react";
+import { useColorMode } from "./components/ui/color-mode";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const { colorMode, toggleColorMode } = useColorMode();
+  // useEffect(() => {
+  //   if (colorMode === "dark") toggleColorMode();
+  // }, [colorMode, toggleColorMode]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button colorPalette="blue">Solid</Button>
+      <Button colorPalette="blue" variant="surface">
+        Surface
+      </Button>
+
+      <Button colorPalette="blue" variant="outline">
+        Outline
+      </Button>
+      <Button colorPalette="blue" variant="ghost">
+        Ghost
+      </Button>
+      <Button colorPalette="blue" variant="plain">
+        Plain
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
