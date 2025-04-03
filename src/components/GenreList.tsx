6c9cfdeb669d81a1f03338ecf9a5d247050303c7
Coookei <89310401+Coookei@users.noreply.github.com>
2025-04-03 12:12:24 +0100

    Show loading skeleton while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,41 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  const skeletons = [1, 2, 3, 4, 5, 6];
+  // if (isLoading) return <Spinner />;
 
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem key={skeleton} paddingY="5px">
+            <Card>
+              <HStack paddingX={3}>
+                <Skeleton boxSize="32px" borderRadius={8}></Skeleton>
+                <CardBody>
+                  <SkeletonText fontSize="lg" />
+                </CardBody>
+              </HStack>
+            </Card>
+          </ListItem>
+        ))}
+
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
