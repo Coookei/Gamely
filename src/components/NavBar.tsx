ccf0fdfe8c4aaa19f9e7ee58b92b21548d239f3f
Coookei <89310401+Coookei@users.noreply.github.com>
2025-04-18 08:33:31 +0100

    Add index link to logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import { HStack } from "@chakra-ui/react";
 import ColourModeSwitch from "./ColourModeSwitch";
 import SearchInput from "./SearchInput";
+import Logo from "./Logo";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
+      <Logo />
       <SearchInput />
       <ColourModeSwitch />
     </HStack>
