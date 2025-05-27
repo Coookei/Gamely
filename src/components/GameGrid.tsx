e28651185e38b5d56183e1e8e29933c015c60fd0
Coookei <89310401+Coookei@users.noreply.github.com>
2025-05-27 20:54:23 +0100

    Fix error when VITE_API_URL .env variable is missing

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -22,7 +22,7 @@ const GameGrid = () => {
   if (error) return <Text>{error.message}</Text>;
 
   const fetchedGamesCount =
-    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+    data?.pages.reduce((total, page) => total + page.results?.length, 0) || 0;
 
   return (
     <InfiniteScroll
